84f1c7e42a8f9c100f20f1f30f144a802a77b841
Nikolay-Sk <69104529+Nikolay-Sk@users.noreply.github.com>
2021-01-11 15:17:59 +0300

    Update task-6.js
    Поправил task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,21 +1,12 @@
 "use strict";
 
-let validationInput = document.getElementById("validation-input");
-let dataLength = validationInput.getAttribute("data-length");
-let parseDataLength = parseInt(dataLength, 10);
-const valInClasslist = validationInput.classList;
+const inputRef = document.getElementById("validation-input");
 
-validationInput.oninput = function () {
-  if (
-    validationInput.value.length !== parseDataLength &&
-    validationInput.value.length !== 0
-  ) {
-    valInClasslist.add("invalid");
-  } else if (validationInput.value.length === parseDataLength) {
-    valInClasslist.add("valid");
-    valInClasslist.remove("invalid");
+inputRef.oninput = () => {
+  inputRef.classList.remove("invalid", "valid");
+  if (inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
   } else {
-    valInClasslist.remove("valid");
-    valInClasslist.remove("invalid");
+    inputRef.classList.add("invalid");
   }
 };
